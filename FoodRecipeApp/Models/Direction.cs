cf8563ab85fa1b3950f58d5c4caf3789d9f404e2
caophuccp <cna0x11@gmail.com>
2020-10-30 13:34:44 +0700

    convert sql to json

--- FoodRecipeApp/Models/Direction.cs
+++ FoodRecipeApp/Models/Direction.cs
@@ -1,19 +1,15 @@
 ﻿
 namespace FoodRecipeApp.Models
 {
-    class Direction
-    {
-        public int ID { get; set; }
-        public int RecipeID { get; set; }
-
-        public string ImageURL { get; set; }
+    public class Direction
+    { 
+        public string ImageName { get; set; }
         public string VideoURL { get; set; }
         public string Instruction { get; set; }
 
-        public int Step { get; set; }
         public override string ToString()
         {
-            return $"Direction:ID-'{ID}' RecipeID-'{RecipeID}' Step-'{Step}' ImageURL-'{ImageURL}' VideoURL-'{VideoURL}' Instruction-'{Instruction}'";
+            return $"Direction: ImageName-'{ImageName}' VideoURL-'{VideoURL}' Instruction-'{Instruction}'";
         }
     }
 }
